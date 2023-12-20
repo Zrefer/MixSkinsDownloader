21bb28eed50eab35e7d3448e485bc3605127f457
Dmitry <zrefer.inc@gmail.com>
2023-12-21 01:22:44 +0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: ['react-app', 'airbnb', 'airbnb-typescript', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        'newlines-between': 'always',
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+  },
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaFeatuers: {
+      jsx: true,
+    },
+  },
+  ignorePatterns: ['/node_modules/', '/build/'],
+};
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 80,
+  useTabs: false,
+  tabWidth: 2,
+  singleQuote: true,
+  semi: true,
+  endOfLine: 'lf',
+  trailingComma: 'all',
+  arrowParens: 'always',
+  bracketSpacing: true,
+};
