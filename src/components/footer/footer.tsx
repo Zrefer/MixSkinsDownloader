5af91a9099bc5b9fced479cf336692dd26fb6c0c
Dmitry Martynov <zrefer.inc@gmail.com>
2025-05-27 01:30:44 +0300

    chore: update copyright, packages and yarn

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,7 +5,8 @@ import styles from './footer.module.css';
 const Footer: FC = function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>Copyright (c) 2023 Zrefer</p>
+      <p>Copyright (c) 2025 Zrefer</p>
+      <p>I love Grayson {'<3'}</p>
       <p>Version: 1.0.0</p>
     </footer>
   );
